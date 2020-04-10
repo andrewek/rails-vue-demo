ee9fc88dbaa42019202079069d8923872ca2c339
Andrew Ek <andrewek@gmail.com>
2020-04-10 15:11:25 -0500

    Add Remote Fetch to Home Page
    Given Axios, our Vue component (`app.vue`), and our endpoint (`GET /api/wombats`), which returns structured JSON, we modified our main app component to display a `ul` of Wombat "records".
    
    Note that `Api::WombatsController`, because it *only* serves JSON, is able to inherit from `ActionController::API` -- this helps to make for slightly speedier requests since we are providing data directly and do onto need to worry about other assets.

--- app/controllers/api/wombats_controller.rb
+++ app/controllers/api/wombats_controller.rb
@@ -2,10 +2,20 @@ class Api::WombatsController < ApiController
   def index
     render json: {
       list: [
-        { name: "Wally Wombat" },
-        { name: "Wilbur Wombat" },
-        { name: "Wendy Wombat" }
+        {
+          name: "Wally Wombat",
+          id: SecureRandom.uuid,
+        },
+        {
+          name: "Wilbur Wombat",
+          id: SecureRandom.uuid,
+        },
+        { name: "Wendy Wombat",
+          id: SecureRandom.uuid,
+        }
       ]
     }, code: :ok
   end
+
+  # render json: some_hash, code: :internal_server_error
 end
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <ul>
+      <li v-for="wombat in wombats" :key="wombat.id">
+        {{wombat.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Hi Vue."
+      message: "Hi Vue.",
+      wombats: []
     }
+  },
+  created() {
+    axios.get(`http://localhost:3000/api/wombats`)
+      .then(response => this.wombats = response.data.list)
+      .catch(error => {
+        console.log(error);
+      })
   }
 }
 </script>
